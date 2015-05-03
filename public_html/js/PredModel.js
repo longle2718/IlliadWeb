4b5b6f73966df927c85b6dccde175256fb6e341c
Long Le <longle1@illinois.edu>
2015-05-02 21:57:20 -0500

    restructure/partition as.html into PredModel.js and as.js. known issue: iw is not displaying the correct tag

---
+++ public_html/js/PredModel.js
@@ -0,0 +1,155 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Long.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+var PredModel = function () {
+    this.alpha;
+    this.bias; // score bias
+    this.kernelFcn;
+    this.kernelScale;
+    this.mu; // standardization mean
+    this.sigma; // standardization sigma
+    this.sv; // support vectos
+    this.svl; // support vector labels
+    this.sxfp; // score transform parameters
+    console.log('model instance created');
+};
+PredModel.prototype = {
+    fromClass: function(classname, cb){
+        var jsonFile;
+        switch (classname){
+            case 'air_conditioner':
+                jsonFile = "data/air_conditioner.json";
+                break;
+            case 'car_horn':
+                jsonFile = "data/car_horn.json";
+                break;
+            case 'children_playing':
+                jsonFile = "data/children_playing.json";
+                break;
+            case 'dog_bark':
+                jsonFile = "data/dog_bark.json";
+                break;
+            case 'drilling':
+                jsonFile = "data/drilling.json";
+                break;
+            case 'engine_idling':
+                jsonFile = "data/engine_idling.json";
+                break;
+            case 'gun_shot':
+                jsonFile = "data/gun_shot.json";
+                break;
+            case 'jackhammer':
+                jsonFile = "data/jackhammer.json";
+                break;
+            case 'siren':
+                jsonFile = "data/siren.json";
+                break;
+            case 'street_music':
+                jsonFile = "data/street_music.json";
+                break;
+            case 'knock':
+                jsonFile = "data/knock.json";
+                break;
+            case 'door_slam':
+                jsonFile = "data/door_slam.json";
+                break;
+            case 'steps':
+                jsonFile = "data/steps.json";
+                break;
+            case 'chair_moving':
+                jsonFile = "data/chair_moving.json";
+                break;
+            case 'key_jingle':
+                jsonFile = "data/key_jingle.json";
+                break;
+            case 'phone_ring':
+                jsonFile = "data/phone_ring.json";
+                break;
+            case 'cough':
+                jsonFile = "data/cough.json";
+                break;
+            case 'speech':
+                jsonFile = "data/speech.json";
+                break;
+        }
+        // save this PredModel to distinguish it from the "this" in getJSON's callback
+        var _this = this;
+        $.getJSON(jsonFile, function(json){
+            _this.fromJSON(json);
+            cb(_this);
+        });
+    },
+    fromJSON: function(json){
+        this.alpha = json.alpha;
+        this.bias = json.bias;
+        this.kernelFcn = json.kernelFcn;
+        this.kernelScale= json.kernelScale;
+        this.mu = json.mu;
+        this.sigma = json.sigma;
+        this.sv = json.sv;
+        this.svl = json.svl;
+        this.xformFcn = json.xformFcn;
+        this.sxfp = json.sxfp;
+    },
+    predict: function(model, feat){
+        if (this.kernelFcn === 'gaussian'){
+            var std_feat = math.dotDivide(math.subtract(feat, model.mu), model.sigma);
+            wx = new Array(model.sv.length);
+            for (k = 0; k < model.sv.length; k++){
+                            wx[k] = model.alpha[k]*model.svl[k]*math.exp(-math.pow(math.norm(math.subtract(model.sv[k], std_feat),2),2)/math.pow(model.kernelScale,2));
+            }
+            score = math.sum(wx)+model.bias;
+
+            if (model.xformFcn === 'sigmoid'){
+                            return sigmoid(score, model.sxfp[0], model.sxfp[1]);
+            }
+            else if (model.xformFcn === 'step'){
+                            return step(score, model.sxfp[0], model.sxfp[1], model.sxfp[2]);
+            }
+            else{
+                            return -1;
+            }
+        }
+        else{
+            console.log("ERROR! unrecognized kernel type." + this.kernelType);
+            return null;
+        }
+    }
+};
+
+var sigmoid = function(x,a,b){
+    return 1/(1+math.exp(a*x+b));
+};
+
+var step = function(x,lb,ub,pi){
+    if (x < lb){
+                    return 0;
+    }
+    else if(x >= lb && x <= ub){
+                    return pi;
+    }
+    else if (x > ub){
+                    return 1;
+    }
+};
---
+++ public_html/js/as.js
@@ -0,0 +1,239 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2015 Long.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+var getBasicQuery = function (){
+   var q = {
+       t1: $("#t1").val(),
+       t2: $("#t2").val()
+   };
+   if ($("#f1").val()){
+       q.f1 = $("#f1").val();
+   }
+   if ($("#f2").val()){
+       q.f2 = $("#f2").val();
+   }
+   if ($("#dur1").val()){
+       q.dur1 = $("#dur1").val();
+   }
+   if ($("#dur2").val()){
+       q.dur2 = $("#dur2").val();
+   }
+   if ($("#lat").val() && $("#lng").val()){
+       q.loc = [$("#lat").val(), $("#lng").val()];
+   }
+   if ($("#rad").val()){
+       q.rad = $("#rad").val();
+   }
+
+   return q;
+};
+
+var IllQueryPost = function (db, user, pwd, col, q, cb_done, cb_fail){
+    //var tZoneOffset = 5/24;
+    var earthRad = 3959; // miles
+    
+    // Construct the query string
+    var params = {'dbname':db, 'colname': col, 'user': user, 'passwd': pwd};
+    if (q.hasOwnProperty('limit')){
+        params.limit = q.limit;
+    }
+    var queryString = $.param(params);
+    var timeDat, freqDat, durDat, lpDat, locDat, kwDat;
+    
+    // Construct the query data to send
+    if (q.hasOwnProperty('t1') && q.hasOwnProperty('t2')){
+        timeDat = '{recordDate:{$gte:{$date:"'+ q.t1+'"}, $lte:{$date:"'+q.t2+'"}}}';
+    }
+    else if (q.hasOwnProperty('t1')){
+        timeDat = '{recordDate:{$gte:{$date:"'+ q.t1+'"}}}';
+    }
+    else if (q.hasOwnProperty('t2')){
+        timeDat = '{recordDate:{$lte:{$date:"'+ q.t2+'"}}}';
+    }
+    
+    if (q.hasOwnProperty('f1') && q.hasOwnProperty('f2')){
+        freqDat = ',{minFreq:{$gte:'+q.f1+'}},{maxFreq:{$lte:'+q.f2+'}}';
+    }
+    else if (q.hasOwnProperty('f1')){
+        freqDat = ',{minFreq:{$gte:'+q.f1+'}}';
+    }else if (q.hasOwnProperty('f2')){
+        freqDat = ',{maxFreq:{$lte:'+q.f2+'}}';
+    }else{
+        freqDat = '';
+    }
+    
+    if (q.hasOwnProperty('dur1') && q.hasOwnProperty('dur2')){
+        durDat = ',{duration:{$gte:'+q.dur1+', $lte:'+q.dur2+'}}';
+    }
+    else if (q.hasOwnProperty('dur1')){
+        durDat = ',{duration:{$gte:'+q.dur1+'}}';
+    }
+    else if (q.hasOwnProperty('dur2')){
+        durDat = ',{duration:{$lte:'+q.dur2+'}}';
+    }
+    else{
+        durDat = '';
+    }
+    
+    if (q.hasOwnProperty('lp1') && q.hasOwnProperty('lp2')){
+        lpDat = ',{logProb:{$gte:'+q.lp1+', $lte:'+q.lp2+'}}';
+    }
+    else if (q.hasOwnProperty('lp1')){
+        lpDat = ',{logProb:{$gte:'+q.lp1+'}}';
+    }
+    else if (q.hasOwnProperty('lp2')){
+        lpDat = ',{logProb:{$lte:'+q.lp2+'}}';
+    }
+    else{
+        lpDat = '';
+    }
+    
+    if (q.hasOwnProperty('loc') && q.hasOwnProperty('rad')){
+        locDat = ',{location:{$geoWithin:{$centerSphere:[['+q.loc[1]+','+q.loc[0]+'], '+q.rad/earthRad+']}}}';
+    }else{
+        locDat = '';
+    }
+    
+    if (q.hasOwnProperty('kw')){
+        kwDat = ',{$text: {$search:"'+q.kw+'"}}';
+    }else{
+        kwDat = '';
+    }
+    
+    // FIX: memcached key is too long
+    var postDat = '{$and:['+timeDat+freqDat+durDat+lpDat+locDat+kwDat+']}';
+    
+    $.ajax({
+        url: 'https://acoustic.ifp.illinois.edu:8081/query?'+queryString,
+        type:'POST',
+        data: postDat,
+        dataType: 'text',
+        timeOut: 10000
+    }).done(function(data){
+        var events = JSON.parse(data);
+        cb_done(events);
+    }).fail(function(){
+        console.log('ajax fail');
+        cb_fail();
+    });
+};
+
+var displayEvent = function(events){
+    for (var i = 0; i <events.length;i++){
+        // Create a marker for each event
+        marker = new google.maps.Marker({
+                        position:new google.maps.LatLng(parseFloat(events[i].location.coordinates[1]), parseFloat(events[i].location.coordinates[0])),
+                        score: events[i].score,
+                        tag: events[i].tag,
+                        recordDate: events[i].recordDate.$date,
+                        filename: events[i].filename,
+                        icon:'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+ (i) +'|FF776B|000000'
+        });
+        marker.setMap(map);
+
+        google.maps.event.addListener(marker, 'mouseover', (function(marker) {
+                        return function(){
+                                        iw.setContent("<p>"+marker.recordDate+"<br>"+marker.score+"<br>"+marker.tag+"</p>");
+                                        iw.open(map, marker);
+                        };
+        })(marker));
+        google.maps.event.addListener(marker, 'mouseout', function(){
+                        iw.close();
+        });
+        oms.addMarker(marker);
+    }
+};
+
+var IllGridfsGet = function(db, user, pwd, gridCol, filename, cb_done, cb_fail){
+    var queryString = $.param({'user':user, 'passwd': pwd, 'filename': filename});
+    
+    $.ajax({
+        url: 'https://acoustic.ifp.illinois.edu:8081/gridfs/'+db+'/'+gridCol+'?'+queryString,
+        type:'GET',
+        dataType :'arraybuffer',
+        timeOut: 10000,
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(data){
+        console.log(data.byteLength);
+        cb_done(data);
+    }).fail(function(){
+        console.log('ajax fail');
+        cb_fail();
+    });
+};
+
+var soundTag = function(data){
+    var audioCtx = new AudioContext();
+    audioCtx.decodeAudioData(data, function(buf){
+            // Play the sound
+            var source = audioCtx.createBufferSource();
+            source.buffer = buf;
+            source.connect(audioCtx.destination);
+            source.start(0);
+
+            // if pred for speech, then invoke ASR (auto speech recog) service for init help
+            /*
+            var ASR_result = '';
+            if ($('input[name="infer"]:checked').val() === "pred" && $("#classes").val() === "speech"){
+                gVoiceApi('AIzaSyD5NvcrQ54Rbzdxpo3FtJsAyvUjy6O3cn4', data, function(xscript){
+                    ASR_result = ' ';
+                    for (var i = 0; i < xscript[2].result[1].alternative.length; i++){
+                        ASR_result = ASR_result+xscript[2].result[1].alternative[i].transcript;
+                    }
+                }, function(){
+                    console.log('ASR failed');
+                });
+            }
+            */
+
+            // Get updated tags from the user
+            var newtag = prompt("Modify tags", marker.tag);
+            if (newtag || marker.tag){
+                            IllWritePost('publicDb', 'publicUser', 'publicPwd', 'event',marker.filename, "set", '{tag:"'+newtag+'"}');
+                            marker.tag = newtag;
+            }
+    }, function(error){
+                    console.log('audio decoding error');
+    });
+};
+
+var IllWritePost = function(db, user, pwd, col, filename, op, field){
+    var queryString = $.param({'dbname':db, 'colname':col, 'user':user, 'passwd': pwd});
+    
+    $.ajax({
+        url: 'https://acoustic.ifp.illinois.edu:8081/write?'+queryString,
+        type:'POST',
+        data: '{filename:"'+filename+'"}\n{$'+op+':'+field+'}',
+        dataType: 'text',
+        timeOut: 10000,
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(data){
+        console.log(data);
+    }).fail(function(){
+        console.log('ajax fail');
+    });
+};
